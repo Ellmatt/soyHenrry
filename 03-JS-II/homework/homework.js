2fae9ddd7021ee3f490e10dd4ea25252d3c77064
matias <pagosptcmatias@gmail.com>
2022-11-09 00:33:26 -0300

    me falta el ejercicio de esDiezOCinco

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -53,7 +53,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  
+  switch(idioma){
+    case 'aleman': return "Guten Tag!";
+    break;
+    case 'mandarin': return "Ni Hao!";
+    break;
+    case 'ingles': return "Hello!"
+    break
+   
+    default: return "Hola!"
+
+  }
+
 }
 
 function colors(color) {
@@ -64,18 +75,41 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case 'blue': return "This is blue";
+    break;
+    case 'red': return "This is red";
+    break;
+    case 'green': return "This is green"
+    break
+    case 'orange':return "This is orange";
+    break
+   
+    default: return "Color not found"
+
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch(numero === 10 || 5){
+    case 1: return true
+    break
+    default: return false
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero<50 && numero>20){
+    return true
+  }else{
+    return false
+  }
 }
 
 function esEntero(numero) {
